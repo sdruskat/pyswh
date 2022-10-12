f5451cb275ca3e7170a83a16ae70b58e85da7269
Stephan Druskat <stephan.druskat@dlr.de>
2022-10-12 16:23:59 +0200

    Test backoff method

--- test/pyswh_test/test_swh.py
+++ test/pyswh_test/test_swh.py
@@ -1,8 +1,11 @@
 # SPDX-FileCopyrightText: 2022 Stephan Druskat <pyswh@sdruskat.net>
 #
 # SPDX-License-Identifier: MIT
+import time
 
 import pytest
+import responses
+import requests
 
 from pyswh import swh
 # from pyswh.errors import SwhSaveError
@@ -16,3 +19,15 @@ from pyswh import swh
     ])
 def test_prepare_url(test_input, expected):
     assert swh._prepare_url(test_input) == expected
+
+
+@responses.activate
+def test_back_off():
+
+    current_epoch = int(time.time())
+    responses.add(responses.GET, 'http://my.api/api/1/foobar',
+                  headers={'X-RateLimit-Reset': str(current_epoch + 5)},  # Rate limit will be "reset" in 2 secs.
+                  status=200)
+    response = requests.get('http://my.api/api/1/foobar')
+    swh._back_off(response)
+    assert int(time.time() - current_epoch) == 7  # 2 secs. added in MOT
