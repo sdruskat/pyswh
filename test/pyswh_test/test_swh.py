17fa4c37f1b03dde70135ee7e4fc5ffe12f5642e
Stephan Druskat <stephan.druskat@dlr.de>
2022-10-12 16:30:44 +0200

    Test with https URLs
    - Otherwise, static code check will raise a security issue

--- test/pyswh_test/test_swh.py
+++ test/pyswh_test/test_swh.py
@@ -25,9 +25,9 @@ def test_prepare_url(test_input, expected):
 def test_back_off():
 
     current_epoch = int(time.time())
-    responses.add(responses.GET, 'http://my.api/api/1/foobar',
+    responses.add(responses.GET, 'https://my.api/api/1/foobar',
                   headers={'X-RateLimit-Reset': str(current_epoch + 5)},  # Rate limit will be "reset" in 2 secs.
                   status=200)
-    response = requests.get('http://my.api/api/1/foobar')
+    response = requests.get('https://my.api/api/1/foobar')
     swh._back_off(response)
     assert int(time.time() - current_epoch) == 7  # 2 secs. added in MOT
