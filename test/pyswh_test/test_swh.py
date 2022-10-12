f4691ee83f35f439aa3790eda8d9db9a7fed9096
Stephan Druskat <stephan.druskat@dlr.de>
2022-10-12 15:58:37 +0200

    Add unit tests for swh._prepare_url

--- test/pyswh_test/test_swh.py
+++ test/pyswh_test/test_swh.py
@@ -2,7 +2,17 @@
 #
 # SPDX-License-Identifier: MIT
 
-# import pytest
-#
-# from pyswh import swh
-# from pyswh.errors import SwhSaveError
+import pytest
+
+from pyswh import swh
+from pyswh.errors import SwhSaveError
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ('abc', 'abc/'),
+    ('abc/', 'abc/'),
+    ('https://www.domain.tld/dir', 'https://www.domain.tld/dir/'),
+    ('https://www.domain.tld/dir/', 'https://www.domain.tld/dir/')
+    ])
+def test_prepare_url(test_input, expected):
+    assert swh._prepare_url(test_input) == expected
