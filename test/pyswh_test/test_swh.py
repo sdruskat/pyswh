3ce476d74b0f1740766aabbd2285b4dc75b6ab5e
Stephan Druskat <stephan.druskat@dlr.de>
2022-10-13 11:11:00 +0200

    Add tests for rate limit checking

--- test/pyswh_test/test_swh.py
+++ test/pyswh_test/test_swh.py
@@ -26,8 +26,35 @@ def test_back_off():
 
     current_epoch = int(time.time())
     responses.add(responses.GET, 'https://my.api/api/1/foobar',
-                  headers={'X-RateLimit-Reset': str(current_epoch + 5)},  # Rate limit will be "reset" in 2 secs.
+                  headers={'X-RateLimit-Reset': str(current_epoch + 2)},  # Rate limit will be "reset" in 2 secs.
                   status=200)
     response = requests.get('https://my.api/api/1/foobar')
     swh._back_off(response)
-    assert int(time.time() - current_epoch) == 7  # 2 secs. added in MOT
+    assert int(time.time() - current_epoch) == 4  # 2 secs. added in MOT
+
+
+@responses.activate
+def test_check_rate_limit_pass(caplog):
+    responses.add(responses.GET, 'https://archive.softwareheritage.org/api/1/ping/',
+                  headers={'X-RateLimit-Remaining': str(1)})
+    swh._check_rate_limit()
+    assert not caplog.text
+
+
+@responses.activate
+def test_check_rate_limit_429(caplog):
+    current_epoch = int(time.time())
+    responses.add(responses.GET, 'https://archive.softwareheritage.org/api/1/ping/', status=429,
+                  headers={'X-RateLimit-Reset': str(current_epoch + 2)})
+    swh._check_rate_limit()
+    assert int(time.time() - current_epoch) == 4  # 2 secs. added in MOT
+
+
+@responses.activate
+def test_check_rate_limit_rate_limit(caplog):
+    current_epoch = int(time.time())
+    responses.add(responses.GET, 'https://archive.softwareheritage.org/api/1/ping/',
+                  headers={'X-RateLimit-Remaining': str(0),
+                           'X-RateLimit-Reset': str(current_epoch + 2)})
+    swh._check_rate_limit()
+    assert int(time.time() - current_epoch) == 4  # 2 secs. added in MOT
